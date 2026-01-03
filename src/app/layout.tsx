299bd85531a1ec0b5953de02f95dbc1d0329a983
fabyolafc <fabyycmp@gmail.com>
2026-01-02 21:47:17 -0300

    feat: add initial restaurant page and menu page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "FSW Donalds",
-  description: "Projeto restaurante!",
+  description: "Bora finalizar esse projeto lindo!",
 };
 
 export default function RootLayout({
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,10 +3,10 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return (
-    <div className="p-5 rounded-xl border border-red-500 ">
-      <h1 className=" text-red-500 p-5">products page</h1>
-      <Button>FSW 7.0</Button> 
-      <Input placeholder="Bora fechar esse projeto!" /> 
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1 className="text-red-500">products page</h1>
+      <Button>FSW 7.0</Button>
+      <Input placeholder="Bora fechar esse projeto!" />
     </div>
   );
 };
