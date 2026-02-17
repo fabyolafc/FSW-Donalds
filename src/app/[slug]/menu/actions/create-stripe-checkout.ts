0af207ba05b64e585e7a66890f088909b35a3322
fabyolafc <fabyycmp@gmail.com>
2026-02-16 22:46:04 -0300

    feat: add product, contexts and orders

--- src/app/[slug]/menu/actions/create-stripe-checkout.ts
+++ src/app/[slug]/menu/actions/create-stripe-checkout.ts
@@ -36,7 +36,7 @@ export const createStripeCheckout = async ({
     },
   });
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-12-15.clover",
+    apiVersion: "2025-02-24.acacia",
   });
   const searchParams = new URLSearchParams();
   searchParams.set("consumptionMethod", consumptionMethod);
